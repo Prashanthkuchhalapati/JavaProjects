57d886a2ea223d34f766b8c9160187f1de20ce84
prashanth <prashanth.kuchhalapati@gmail.com>
2023-06-27 07:48:10 -0500

    first commit

---
+++ A.java
@@ -0,0 +1,18 @@
+package Polymorphism;
+
+public class A {
+	 public static void add() {
+	        System.out.print("super class method ");
+	    }
+}
+
+
+class B extends A {
+    public static void add() {
+        System.out.println("subclass method ");
+    }
+}
+
+
+
+  
---
+++ Mian.java
@@ -0,0 +1,8 @@
+package Polymorphism;
+
+public class Mian {
+	public static void main(String[] args) {
+        B obj = new B();
+        obj.add();
+    }
+}
